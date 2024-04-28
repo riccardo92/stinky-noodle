c0700e3eaf57fb510ffbb656830f8091815dc79d
Riccardo Tiebax <riccardo.tiebax@cz.nl>
2024-04-28 22:29:14 +0200

    tests(test_builtins.py): added casing builtin unit test

--- tests/test_builtins.py
+++ tests/test_builtins.py
@@ -1,9 +1,10 @@
-from typing import Any, List, Optional, Set, Tuple, Union
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 import pytest
 
 from stinky.noodle.utils.builtins import (
     alphabetical,
+    casing,
     enumeration,
     falsy,
     length,
@@ -117,8 +118,10 @@ def test_builtins_length(obj: Any, min_arg: int, max_arg: int, result: bool):
 @pytest.mark.parametrize(
     ("obj", "match", "not_match", "result"),
     [
-        ("some value", "value", None, True),
-        ("some value", "^(value)", None, False),
+        ("some value", ".*(value)$", None, True),
+        ("some value", "^(value).*", None, False),
+        ("some value", None, ".*(value)$", False),
+        ("some value", None, "^(value).*", True),
     ],
 )
 def test_builtins_pattern(
@@ -126,3 +129,55 @@ def test_builtins_pattern(
 ):
     """Test the pattern builtin"""
     assert pattern(obj=obj, match=match, not_match=not_match) is result
+
+
+@pytest.mark.parametrize(
+    ("obj", "type", "disallow_digits", "separator", "result"),
+    [
+        # {"char": "", "allow_leading": True}
+        ("supposedToBeCamelCase", "camel", False, None, True),
+        ("SupposedToBePascalCase", "pascal", False, None, True),
+        ("NotSupposedToBeCamelCase", "camel", False, None, False),
+        ("supposed_to_be_snake_case", "snake", False, None, True),
+        ("not_supposed_to_be_Snake_case", "snake", False, None, False),
+        ("supposed-to-be-kebab-case", "kebab", False, None, True),
+        ("not_supposed-to-be-kebab-case", "kebab", False, None, False),
+        ("SUPPOSED-TO-BE-COBOL-CASE", "cobol", False, None, True),
+        ("NOT_SUPPOSED-TO-BE-COBOL-CASE", "cobol", False, None, False),
+        ("SUPPOSED_TO_BE_MACRO_CASE", "macro", False, None, True),
+        (
+            "#SUPPOSED#TO#BE#MACRO#CASE",
+            "macro",
+            False,
+            {"allow_leading": True, "char": "#"},
+            True,
+        ),
+        (
+            "#SUPPOSED#TO#BE#MACRO#CASE",
+            "macro",
+            False,
+            {"allow_leading": False, "char": "#"},
+            False,
+        ),
+        (
+            "SUPPOSED!TO!BE!MACRO!CASE",
+            "macro",
+            False,
+            {"allow_leading": False, "char": "!"},
+            True,
+        ),
+        ("NOT-SUPPOSED_TO_BE_MACRO_CASE", "macro", False, None, False),
+        ("not-supposed-to-be-macro-case", "macro", False, None, False),
+        ("supposedtobeflatcase", "flat", False, None, True),
+        ("NOTsupposedtobeflatcase", "flat", False, None, False),
+        ("not-supposedtobeflatcase", "flat", False, None, False),
+    ],
+)
+def test_builtins_casing(
+    obj: str, type: str, disallow_digits: bool, separator: Optional[Dict], result: bool
+):
+    """Test the casing builtin"""
+    assert (
+        casing(obj=obj, type=type, disallow_digits=disallow_digits, separator=separator)
+        is result
+    )
