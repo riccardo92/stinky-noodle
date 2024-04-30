3df4df9c9a49b1ea5062602950cd8593c74d5318
Riccardo Tiebax <riccardo.tiebax@cz.nl>
2024-04-30 21:45:28 +0200

    fix(utils/__init__.py): conditional callable prefix

--- src/stinky/noodle/utils/__init__.py
+++ src/stinky/noodle/utils/__init__.py
@@ -1,13 +1,15 @@
 from convert_case import snake_case
 
 
-def sanitize_callable_name(name: str) -> str:
+def sanitize_callable_name(name: str, custom: bool = False) -> str:
     """Sanitzation for the JS callable name.
 
     Args:
         name (str): name of the callable
+        custom(bool): whether the callable is a custom one
 
     Returns:
         str: sanitzed python compatible callable name
     """
-    return f"builtin_{snake_case(name)}"
+    prefix = "" if custom else "builtin_"
+    return f"{prefix}{snake_case(name)}"
